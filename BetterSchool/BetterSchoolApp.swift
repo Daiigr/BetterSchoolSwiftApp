fa15eadef4f64feabb157a86ee8a37308e92c8d6
Daiigr <daiigrbr@gmail.com>
2023-05-07 10:33:45 +0300

    Initial Commit

---
+++ BetterSchool/BetterSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSchoolApp.swift
+//  BetterSchool
+//
+//  Created by Daniel Grbac Bravo on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
